8d95de0c2715c56c8fadc1bd2ca105a26c6623dd
Thomas Eilermann <user@purebase.de>
2022-10-03 23:41:02 +0200

    GooglePhotos Scan: Added process to fetch image url from shared googlePhotos album.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,10 @@
+// @ts-ignore
 import * as functions from "firebase-functions";
+// @ts-ignore
 import {Request, Response} from "firebase-functions";
 import * as admin from "firebase-admin";
 import {seller_initData} from "./init";
+import axios from "axios";
 
 admin.initializeApp();
 
@@ -20,10 +23,52 @@ export const initseller = functions.region('europe-west1').https
     .onRequest((req: Request, res: Response) => {
             const id = req.query['id'] as string;
             if (id === undefined) {
-                res.send({error: "missing seller id", query: req.query});
+                res.send({error: "missing seller id of album", query: req.query});
             } else {
                 seller_initData(id);
                 res.send({success: "alles gut"});
+            }
+        }
+    );
+
+export const addAdobePortfolioAlbum = functions.region('europe-west1').https
+    .onRequest((req: Request, res: Response) => {
+        // https://lightroom.adobe.com/shares/2b2d21abce0243cd94b163038dc4bf2b
+    });
+
+// aZtFGHwk86FJUtxb7
+export const addGPhotoAlbum = functions.region('europe-west1').https
+    .onRequest((req: Request, res: Response) => {
+            const id = req.query['id'] as string;
+            if (id === undefined) {
+                res.send({error: "addGPhotoAlbum: missing album id for parsing", query: req.query});
+            } else {
+                const regex = /\["(https:\/\/lh3\.googleusercontent\.com\/[a-zA-Z0-9\-_]*)"/g;
+
+                axios.get('https://photos.app.goo.gl/' + id)
+                    .then(async response => {
+
+                        const images:{url:string}[] = [];
+
+                        let match;
+                        while (match = regex.exec(response.data)) {
+                            const url = match[1];
+                            images.push({url: url});
+                        }
+
+                        await admin.firestore()
+                                .collection('ALBUMS')
+                                .doc(id).set({
+                                    children: images
+                                }
+                            );
+
+                        res.send({"album image count": images.length});
+                    })
+                    .catch(reason => {
+                        console.error(reason);
+                        res.send({"error": reason});
+                    });
             }
         }
     );
